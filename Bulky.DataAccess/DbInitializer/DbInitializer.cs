5b755ee81e35ee787a49727986440795a7859974
huylebron <doquochuy27102003@gmail.com>
2024-10-28 16:04:23 +0700

    fix sonar

--- Bulky.DataAccess/DbInitializer/DbInitializer.cs
+++ Bulky.DataAccess/DbInitializer/DbInitializer.cs
@@ -3,15 +3,11 @@ using BulkyBook.Models;
 using BulkyBook.Utility;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace BulkyBook.DataAccess.DbInitializer {
-    public class DbInitializer : IDbInitializer {
-
+namespace BulkyBook.DataAccess.DbInitializer
+{
+    public class DbInitializer : IDbInitializer
+    {
         private readonly UserManager<IdentityUser> _userManager;
         private readonly RoleManager<IdentityRole> _roleManager;
         private readonly ApplicationDbContext _db;
@@ -19,36 +15,42 @@ namespace BulkyBook.DataAccess.DbInitializer {
         public DbInitializer(
             UserManager<IdentityUser> userManager,
             RoleManager<IdentityRole> roleManager,
-            ApplicationDbContext db) {
+            ApplicationDbContext db)
+        {
             _roleManager = roleManager;
             _userManager = userManager;
             _db = db;
         }
 
 
-        public void Initialize() {
-
-
-            //migrations if they are not applied
-            try {
-                if (_db.Database.GetPendingMigrations().Count() > 0) {
+        public void Initialize()
+        {
+            try
+            {
+                if (_db.Database.GetPendingMigrations().Count() > 0)
+                {
                     _db.Database.Migrate();
                 }
             }
-            catch(Exception ex) { }
-
+            catch (Exception ex)
+            {
+            }
 
 
             //create roles if they are not created
-            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult()) {
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter().GetResult();
-                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter().GetResult();
+            if (!_roleManager.RoleExistsAsync(SD.Role_Customer).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Customer)).GetAwaiter()
+                             .GetResult();
+                _roleManager.CreateAsync(new IdentityRole(SD.Role_Employee)).GetAwaiter()
+                             .GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Admin)).GetAwaiter().GetResult();
                 _roleManager.CreateAsync(new IdentityRole(SD.Role_Company)).GetAwaiter().GetResult();
 
 
                 //if roles are not created, then we will create admin user as well
-                _userManager.CreateAsync(new ApplicationUser {
+                _userManager.CreateAsync(new ApplicationUser
+                {
                     UserName = "admin@dotnetmastery.com",
                     Email = "admin@dotnetmastery.com",
                     Name = "Bhrugen Patel",
@@ -60,9 +62,9 @@ namespace BulkyBook.DataAccess.DbInitializer {
                 }, "Admin123*").GetAwaiter().GetResult();
 
 
-                ApplicationUser user = _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@dotnetmastery.com");
+                ApplicationUser user =
+                    _db.ApplicationUsers.FirstOrDefault(u => u.Email == "admin@dotnetmastery.com");
                 _userManager.AddToRoleAsync(user, SD.Role_Admin).GetAwaiter().GetResult();
-
             }
 
             return;
